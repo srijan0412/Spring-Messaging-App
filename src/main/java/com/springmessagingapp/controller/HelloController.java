3f8614c882b86b32b789a423a2d731b3370ee708
srijan0412 <srijan.soni01@gmail.com>
2025-02-21 20:13:54 +0530

    [Srijan] Add . Say Hello Mark!

--- src/main/java/com/springmessagingapp/controller/HelloController.java
+++ src/main/java/com/springmessagingapp/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.springmessagingapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,10 @@ public class HelloController {
     @RequestMapping("/hello")
     public String sayHello() {
         return "Hello";
+    }
+
+    @RequestMapping(value = {"/hello/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
     }
 }
