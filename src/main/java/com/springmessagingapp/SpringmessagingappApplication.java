74dd529dcf2c17ffb2d4332cb0463528fc089c92
srijan0412 <srijan.soni01@gmail.com>
2025-02-21 18:45:22 +0530

    [Srijan] Initial Commit

---
+++ src/main/java/com/springmessagingapp/SpringmessagingappApplication.java
@@ -0,0 +1,13 @@
+package com.springmessagingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmessagingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmessagingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmessagingapp/SpringmessagingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmessagingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmessagingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
